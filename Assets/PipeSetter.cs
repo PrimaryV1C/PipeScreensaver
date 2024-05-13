605bd2f5cffcafb6ab0519500a579da484798e13
PrimaryV1C <petrovictorelias@gmail.com>
2024-05-13 08:35:16 +0200

    Added function that generates a new pipe

--- Assets/PipeSetter.cs
+++ Assets/PipeSetter.cs
@@ -34,5 +34,34 @@ public class PipeSetter : MonoBehaviour
 
         GameObject newPipe = Instantiate(pipePrefab, startPosition, rotation);
         newPipe.GetComponent<Renderer>().material.color = currentColor;
+private void NewPipe(){
+
+    iterationCap++;
+
+    currentColor = pipeColors[Random.Range(0, pipeColors.Length)]; //Picks a new random color from the color list
+
+    //Randomly selects a new point withinn the bounds
+    int x = Random.Range((int)spaceBounds.min.x, (int)spaceBounds.max.x + 1);
+    int y = Random.Range((int)spaceBounds.min.y, (int)spaceBounds.max.y + 1);        
+    int z = Random.Range((int)spaceBounds.min.z, (int)spaceBounds.max.z + 1);
+    Vector3 randomPosition = new Vector3(x, y, z);
+
+    // Check if there is a pipe at the random position
+    Collider[] colliders = Physics.OverlapSphere(randomPosition, 1f); // Adjust radius as needed
+
+    foreach (Collider col in colliders)
+    {
+        if (col.gameObject.CompareTag("Pipe"))
+        {
+            // If there is a pipe at the position, recursively call this method to find a new position
+            if(iterationCap < 100){ // Ends the search after 100 iterations
+                NewPipe();
+            }
+         }
+    }
+
+    startPosition = randomPosition; //Updates the startPosition
+    newPipe = false;
+
     }
 }
